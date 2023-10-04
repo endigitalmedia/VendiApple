662f27e8bc17df2c4d12fcc90f2de201ad3c0f31
endigitalmedia <endigitalmedia@gmail.com>
2023-10-04 18:13:49 -0400

    Initial Commit

---
+++ Shared/VendiAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendiAppleApp.swift
+//  Shared
+//
+//  Created by Sherome Hardison on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct VendiAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
